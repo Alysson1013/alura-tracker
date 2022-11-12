286425dcefc0710a06dce402ac12c0d9a62170e1
Alysson Eduardo <alysson6801@hotmail.com>
2022-11-12 16:38:00 -0300

    refactor: move timer to new componenet

---
+++ src/components/Cronometro.vue
@@ -0,0 +1,28 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <section>
+    <strong>
+      {{ tempoDecorrido }}
+    </strong>
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: 'Cronometro',
+  props: {
+    tempoEmSegundos: {
+      type: Number,
+      default: 0
+    }
+  },
+  //monitora a informação
+  computed: {
+    tempoDecorrido(): string {
+      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+    }
+  },
+})
+</script>
--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -7,9 +7,7 @@
       </div>
       <div class="column">
         <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <section>
-            <strong>{{ tempoDecorrido }}</strong>
-          </section>
+          <Cronometro :tempo-em-segundos="tempoEmSegundos" />
           <button class="button" @click="iniciar">
             <span class="icon">
               <i class="fas fa-play"></i>
@@ -30,20 +28,18 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import Cronometro from '@/components/Cronometro.vue'
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
   name: "Formulario",
+  components: {
+    Cronometro
+  },
   data () {
     return {
       tempoEmSegundos: 0,
       cronometro: 0
-    }
-  },
-  //Monitora a informação
-  computed: {
-    tempoDecorrido(): string {
-      return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
     }
   },
   methods: {
