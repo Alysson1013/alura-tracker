ecf65dc3f3e66202294ec53bf2a4b1dad9df7ec5
Alysson Eduardo <alysson6801@hotmail.com>
2022-11-19 16:13:53 -0300

    feat: create navbar

---
+++ src/views/Projetos.vue
@@ -0,0 +1,16 @@
+<template lang="">
+  <div>
+    <h1>Projetos</h1>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  // eslint-disable-next-line vue/multi-word-component-names
+  name: "Projetos"
+})
+</script>
+<style lang="scss">
+  
+</style>
