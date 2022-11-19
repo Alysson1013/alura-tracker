8befbe702049f472ba6e15d2fe0dbc202d8c27e6
Alysson Eduardo <alysson6801@hotmail.com>
2022-11-19 18:19:04 -0300

    feat: add state

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -3,25 +3,32 @@
 <template>
   <div class="box formulario">
     <div class="columns">
-      <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+      <div class="column is-5" role="form" aria-label="Formulário para criação de uma nova tarefa">
         <!-- v-model linka o valor do input ao estado de data  -->
-        <input 
-          type="text"
-          class="input" 
-          placeholder="Qual tarefa você deseja iniciar?"
-          v-model="descricao"
-        >
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" v-model="descricao">
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option value="">Selecione o projeto</option>
+            <option :value="projeto.id" v-for="projeto in projetos" :key="projeto.id">
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
       </div>
       <div class="column">
-       <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Temporizador from '@/components/Temporizador.vue'
+import { useStore } from 'vuex';
+import { key } from '@/store';
 
 export default defineComponent({
   // eslint-disable-next-line vue/multi-word-component-names
@@ -30,16 +37,25 @@ export default defineComponent({
   components: {
     Temporizador
   },
-  data () {
+  data() {
     return {
-      descricao: ''
+      descricao: '',
+      idProjeto: ''
+    }
+  },
+  setup() {
+    const store = useStore(key)
+
+    return {
+      projetos: computed(() => store.state.projetos)
     }
   },
   methods: {
     finalizarTarefa(tempoDecorrido: number): void {
       this.$emit('aoSalvarTarefa', {
         duracaoEmSegundos: tempoDecorrido,
-        descricao: this.descricao
+        descricao: this.descricao,
+        projeto: this.projetos.find(prj => prj.id === this.idProjeto)
       })
       this.descricao = ''
     }
@@ -48,8 +64,8 @@ export default defineComponent({
 </script>
 
 <style>
-  .formulario {
-    color: var(--texto-primario);
-    background-color: var(--bg-primario);
-  }
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
 </style>
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@fortawesome/fontawesome-free/css/all.css'
 import router from './roteador'
+import { key, store } from './store'
 
 createApp(App)
-    .use(router)    
+    .use(router)   
+    .use(store, key)
     .mount('#app')
